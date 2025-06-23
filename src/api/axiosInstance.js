ae38d38d17b6c0ee617e83efde9b264a146e00c3
abhinprasad666 <abhinprasad866@gmail.com>
2025-06-24 01:45:25 +0530

    fix: corrected VITE_API_BASE_URL env variable name

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create instance
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_UR || "http://localhost:5000", // Use env file for URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", // Use env file for URL
   withCredentials: true, // To send cookies
   headers: {
     "Content-Type": "application/json",
